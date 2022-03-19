c18f8f5764054a23f96278b3bfa52f4bd01935f7
nmin11 <nmin1124@gmail.com>
2022-03-19 18:31:56 +0900

    check problem

--- server/src/main/java/com/Yana/Buddy/BuddyApplication.java
+++ server/src/main/java/com/Yana/Buddy/BuddyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BuddyApplication {
-
+	// 기능 점검용 커밋
 	public static void main(String[] args) {
 		SpringApplication.run(BuddyApplication.class, args);
 	}
